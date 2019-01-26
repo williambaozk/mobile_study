cef500d90944c7b3b1cd42a8d0f9c7f25558d7cf
williambaozk <williambaozk@foxmail.com>
2019-01-26 17:05:48 +0800

    init

---
+++ src/main/java/com/study/mobile_study/MobileStudyApplication.java
@@ -0,0 +1,14 @@
+package com.study.mobile_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobileStudyApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/study/mobile_study/MobileStudyApplicationTests.java
@@ -0,0 +1,17 @@
+package com.study.mobile_study;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MobileStudyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
