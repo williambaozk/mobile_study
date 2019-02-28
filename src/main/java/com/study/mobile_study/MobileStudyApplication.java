124ae71d48bb764a4fbbdcab7bc59cc741cc954d
williambaozk <williambaozk@foxmail.com>
2019-02-28 17:03:58 +0800

    database

--- src/main/java/com/study/mobile_study/MobileStudyApplication.java
+++ src/main/java/com/study/mobile_study/MobileStudyApplication.java
@@ -1,9 +1,11 @@
 package com.study.mobile_study;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan({"com.study.mobile_study.mapper"})
 public class MobileStudyApplication {
 
     public static void main(String[] args) {
